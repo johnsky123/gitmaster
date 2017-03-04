b7f0260ecc67364116be8a1264eecea19f1c1419
徐亚东 <yadong.xu@palmaplus.com>
2017-03-04 15:57:46 +0800

    first push

---
+++ src/components/Example.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from 'antd-mobile/lib/button';
+
+
+const Example = () => {
+  return (
+    <div>
+      Example
+      <Button type="primary" size="large">
+
+    33333
+      </Button>
+      
+     
+    </div>
+  );
+};
+
+Example.propTypes = {
+};
+
+export default Example;
---
+++ src/routes/IndexPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.css';
+import Button from 'antd-mobile/lib/button';
+
+
+const IndexPage = () =>
+
+ 
+<div className={styles.normal}>
+   1111
+</div>
+ 
+    
+ 
+ 
+
+/*function IndexPage() {
+  return (
+    <div className={styles.normal}>
+       111
+    </div>
+  );
+}*/
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
